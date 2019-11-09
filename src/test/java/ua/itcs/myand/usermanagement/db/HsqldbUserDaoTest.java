9554fb74c1b7aa202098d4cff955ea3f6c15dbaa
Myand Denis <denys.miand@nure.ua>
2019-11-09 17:56:04 +0200

    Adding of Factory into Dao-object constructor

--- src/test/java/ua/itcs/myand/usermanagement/db/HsqldbUserDaoTest.java
+++ src/test/java/ua/itcs/myand/usermanagement/db/HsqldbUserDaoTest.java
@@ -1,15 +1,34 @@
 package ua.itcs.myand.usermanagement.db;
 
+import java.util.Date;
+
 import junit.framework.TestCase;
+import ua.itcs.myand.usermanagement.User;
 
 public class HsqldbUserDaoTest extends TestCase {
-
+	private HsqldbUserDao dao;
+	private ConnectionFactory connectionFactory;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
+		connectionFactory = new ConnectionFactoryImpl();
+		dao = new HsqldbUserDao(connectionFactory);
 	}
 
 	public void testCreate() {
-		fail("Not yet implemented");
+		try {
+			User user = new User();
+			user.setFirstName("John");
+			user.setLastName("Doe");
+			user.setDateOfBirthd(new Date());
+			assertNull(user.getId());
+			user = dao.create(user);
+			assertNotNull(user);
+			assertNotNull(user.getId());
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
 	}
 
 }
