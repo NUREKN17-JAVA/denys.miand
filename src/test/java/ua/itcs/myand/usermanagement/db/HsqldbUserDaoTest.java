335b3abfc4e99ef317c3b82e18d45a8647d8be6a
Myand Denis <denys.miand@nure.ua>
2019-11-09 17:25:30 +0200

    Create method's test

---
+++ src/test/java/ua/itcs/myand/usermanagement/db/HsqldbUserDaoTest.java
@@ -0,0 +1,15 @@
+package ua.itcs.myand.usermanagement.db;
+
+import junit.framework.TestCase;
+
+public class HsqldbUserDaoTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+}
