0690794f10fb2a9ae3838633420533049965393d
Myand Denis <denys.miand@nure.ua>
2019-11-09 18:21:14 +0200

    Create method's implementation

--- src/main/java/ua/itcs/myand/usermanagement/db/HsqldbUserDao.java
+++ src/main/java/ua/itcs/myand/usermanagement/db/HsqldbUserDao.java
@@ -1,14 +1,40 @@
 package ua.itcs.myand.usermanagement.db;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
+
 
 import ua.itcs.myand.usermanagement.User;
 
 public class HsqldbUserDao implements UserDao {
+	
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
+	private ConnectionFactory connectionFactory;
+	
+	public HsqldbUserDao(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
 
 	public User create(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(3, new Date(user.getDateOfBirthd().getTime()));
+			int n = statement.executeUpdate();
+			if (n != 1) {
+				throw new DatabaseException("Number of the inserted rows: " + n);
+			}
+			return null;
+		} catch (DatabaseException e) {
+			throw e;
+		} catch (SQLException e) {
+			throw new DatabaseException();
+		}
 	}
 
 	public void update(User user) throws DatabaseException {
