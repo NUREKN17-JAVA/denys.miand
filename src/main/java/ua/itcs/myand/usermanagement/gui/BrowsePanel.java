717f7a3b64640d6327d96bebcd93efecfcf7b11b
Myand Denis <denys.miand@nure.ua>
2019-12-07 19:20:29 +0200

    Created main window and main control panel

---
+++ src/main/java/ua/itcs/myand/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,11 @@
+package ua.itcs.myand.usermanagement.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+
+	public BrowsePanel(MainFrame frame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
