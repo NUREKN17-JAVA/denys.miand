a0bc5f74d7f2ce3c99dd14fef83b87cb3b687c17
Myand Denis <denys.miand@nure.ua>
2019-12-07 18:32:31 +0200

    Created a test class inherited from JFCTestCase

---
+++ src/main/java/ua/itcs/myand/usermanagement/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package ua.itcs.myand.usermanagement.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
