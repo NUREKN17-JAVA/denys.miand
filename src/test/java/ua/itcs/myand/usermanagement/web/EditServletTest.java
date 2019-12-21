005626a7169f385db00d73734728da3cc39f93bc
Myand Denis <denys.miand@nure.ua>
2019-12-21 21:22:09 +0200

    Created an editing servlet

---
+++ src/test/java/ua/itcs/myand/usermanagement/web/EditServletTest.java
@@ -0,0 +1,28 @@
+package ua.itcs.myand.usermanagement.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import ua.itcs.myand.usermanagement.User;
+
+public class EditServletTest extends MockServletTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		createServlet(EditServlet.class); 
+	}
+
+	public void testEdit() {
+		Date date = new Date(); 
+		User user = new User(new Long(1000), "John", "Doe", date);
+		getMockUserDao().expect("update", user);
+		
+		addRequestParameter("id", "1000");
+		addRequestParameter("firstName", "John");
+		addRequestParameter("lastName", "Doe");
+		addRequestParameter("date", DateFormat.getDateInstance().format(date));
+		addRequestParameter("okButton", "Ok");
+		doPost();
+	}
+	
+}
